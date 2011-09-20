9803300098f811981acc03b26a71500d636d2a02
Juan C. Müller <juan@challengepost.com>
2011-09-20 15:11:14 -0400

    Kicks ass!

--- generate-kb.sh
+++ generate-kb.sh
@@ -1,20 +1,33 @@
+#!/bin/bash
+
 echo $(pwd)
-git checkout master && git pull && \
-  git submodule init && git submodule update && \
-  ( \
-    cd _site && git checkout master && git pull \
-  ) && \
-  gollum-site generate && \
-  ( \
-    cd _site && git checkout master && \
-  ( \
-    [[ 0 -eq "`git diff | wc -l`" ]] && echo "No changes." || git add . && git commit -m "Generated newest site." && git push \
-  ) || echo "Positive reinforcement" \
-) && \
-( \
-  [[ 0 -eq "`git diff | wc -l`" ]] && echo "No changes." || \
-  ( \
-    git commit -am "Updated _site" && git push \
-  ) || echo "Positive reinforcement" \
-)
+
+git checkout master
+git pull
+git submodule init
+git submodule update
+
+(cd _site && git checkout master && git pull)
+
+gollum-site generate
+
+cd _site
+git checkout master
+if [[ 0 -eq "`git diff | wc -l`" ]]
+then
+  echo "No changes."
+else
+  git add .
+  git commit -m "Generated newest site."
+  git push
+fi
+cd ..
+
+if [[ 0 -eq "`git diff | wc -l`" ]]
+then
+  echo "No changes."
+else
+  git commit -am "Updated _site"
+  git push
+fi
 
